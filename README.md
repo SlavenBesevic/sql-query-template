65440628247baa3d7ff75f25920a5238c4515bbe
SlavenBesevic <slavenbesevic@gmail.com>
2023-06-29 14:12:34 +0200

    cjs

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ```bash
 npm i sql-query-template
 ```
+
+### Thanks to:
+
+https://snyk.io/blog/best-practices-create-modern-npm-package/
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export const sql = (query: TemplateStringsArray, ...values: any[]) => {
-  const queryArray: string[] = [...query];
-
+export const sql = (originalText: TemplateStringsArray, ...values: any[]) => {
+  const originalTextArray: string[] = [...originalText];
   let text = "";
 
   if (values.length) {
@@ -8,29 +7,36 @@ export const sql = (query: TemplateStringsArray, ...values: any[]) => {
     while (i < values.length) {
       if (values[i]?._sqlQueryTagFunction) {
         const newValues = values[i].values;
-        const newText = values[i].queryArray;
+        const newText = values[i].originalText;
         const newTextLength = newText.length;
 
         values.splice(i, 1);
         values.splice(i, 0, ...newValues);
 
-        queryArray.splice(i + 1, 0, ...newText);
+        originalTextArray.splice(i + 1, 0, ...newText);
 
-        queryArray[i + newTextLength] = `${queryArray[i + newTextLength]} ${
-          queryArray[i + newTextLength + 1]
+        originalTextArray[i + newTextLength] = `${
+          originalTextArray[i + newTextLength]
+        } ${originalTextArray[i + newTextLength + 1]}`;
+        originalTextArray.splice(i + newTextLength + 1, 1);
+
+        originalTextArray[i] = `${originalTextArray[i]} ${
+          originalTextArray[i + 1]
         }`;
-        queryArray.splice(i + newTextLength + 1, 1);
-
-        queryArray[i] = `${queryArray[i]} ${queryArray[i + 1]}`;
-        queryArray.splice(i + 1, 1);
+        originalTextArray.splice(i + 1, 1);
       }
-      text += `${queryArray[i]}$${i + 1}`;
+      text += `${originalTextArray[i]}$${i + 1}`;
       i += 1;
     }
-    text += queryArray.slice(-1);
+    text += originalTextArray.slice(-1)[0];
   } else {
-    text = queryArray[0] as string;
+    text = originalTextArray[0] as string;
   }
 
-  return { text, values, originalText: query, _sqlQueryTagFunction: true };
+  return {
+    text,
+    values,
+    originalText: originalTextArray,
+    _sqlQueryTagFunction: true,
+  };
 };
